d8886da5ad34c583594db7271e63f2e35be34e96
walid-wow <medali25tech@gmail.com>
2023-07-14 23:54:35 +0000

    H

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+	*main - entry ponit
+	*Description: task2
+	*Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int digit = 0;
+
+	while (digit <= 9)
+	{
+		/*couvert digit to ASCII representetion*/
+		putchar(digit + '0');
+		digit++;
+	}
+	putchar("\n");
+
+	return (0);
+}
