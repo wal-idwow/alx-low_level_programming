f00acff3d2f1b7657b0bcca732ca1974abb334bd
walid-wow <medali25tech@gmail.com>
2023-07-15 00:04:36 +0000

    H

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+	* main - Entry point
+	* Description: task 7
+	* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char ch = 'z';
+
+	while (ch >= 'a')
+	{
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+
+	return (0);
+}
