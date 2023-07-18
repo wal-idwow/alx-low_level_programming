fbcd1198d88e30c664aa7ec69b6b4d6760a3cac2
walid-wow <medali25tech@gmail.com>
2023-07-18 19:17:45 +0000

    Done

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -13,6 +13,7 @@ void print_times_table(int n)
 	{
 		for (num = 0; num <= n; num++)
 		{
+			_putchar(48);
 			for (mult = 1; mult <= n; mult++)
 			{
 				_putchar(',');
@@ -21,9 +22,9 @@ void print_times_table(int n)
 				prod = num * mult;
 
 				if (prod <= 9)
-					_putchar('  ');
+					_putchar(' ');
 				if (prod <= 99)
-					_putchar('  ');
+					_putchar(' ');
 				if (prod >= 100)
 				{
 					_putchar((prod / 100) + 48);
