754d134d34c710c86713eaa4e775e444cf1a0151
walid-wow <medali25tech@gmail.com>
2023-07-20 23:05:42 +0000

    done

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -8,12 +8,21 @@
 
 void print_square(int size)
 {
-	int row, column;
-
-	for (row = 1; row <= size; row++)
+	if (size <= 0)
 	{
-		for (column = 1; column <= size; column++)
-			_putchar('#');
 		_putchar('\n');
+	}
+	else
+	{
+		int i, j;
+
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
 	}
 }
