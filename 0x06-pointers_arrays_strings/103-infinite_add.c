394afff3257d018d8e4dacbba233a5a9adecd339
walid-wow <medali25tech@gmail.com>
2023-07-28 15:23:22 +0000

    done

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,86 +1,77 @@
-#include "main.h"
-
 /**
- * _strlen - computes the length of a string
- * @str: input string
- * Return: length of the string
- */
-int _strlen(char *str)
+	* rev_string - reverse_array
+	* @n: integer parameters
+	* Return: 0
+*/
+
+void rev_string(char *n)
 {
-	int len = 0;
 
-	while (str[len] != '\0')
-		len++;
-	return (len);
-}
+	int i = 0;
+	int j = 0;
+	char temp;
 
-/**
- * reverse_string - reverses a string
- * @str: input string
- * @len: length of the string
- */
-void reverse_string(char *str, int len)
-{
-	int start = 0;
-	int end = len - 1;
-
-	while (start < end)
-
+	while (*(n + i) != '\0')
 	{
-		char temp = str[start];
+		i++;
+	}
+	i++;
 
-		str[start] = str[end];
-		str[end] = temp;
-		start++;
-		end--;
+	for (j = 0; j < i; j++, i--)
+	{
+		temp = *(n + j);
+		*(n + j) = *(n + i);
+		*(n + i) = temp;
 	}
 }
 
 /**
- * infinite_add - adds two numbers
- * @n1: first number
- * @n2: second number
- * @r: result buffer
- * @size_r: buffer size
- *
- * Return: Pointer to the result
- */
+	* infinite_add - add 2 numbers together
+	* @n1: text representation of 1st number to add
+	* @n2: text representation of 2nd number to add
+	* @r: pointer to buffer
+	* @size_r: buffer size
+	* Return: pointer to calling function
+*/
+
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int len1, len2, carry, sum, i, j;
+	int overflow = 0, i = 0, j = 0, digits = 0;
+	int val1 = 0, val2 = 0, temp_tot = 0;
 
-	len1 = _strlen(n1);
-	len2 = _strlen(n2);
-
-	if (size_r <= len1 || size_r <= len2)
+	while (*(n1 + i) != '\0')
+		i++;
+	while (*(n2 + j) != '\0')
+		j++;
+	i--;
+	j--;
+	if (j >= size_r || i >= size_r)
 		return (0);
-
-	i = len1 - 1;
-	j = len2 - 1;
-	carry = 0;
-	r[size_r] = '\0';
-
-	while (i >= 0 || j >= 0 || carry)
+	while (j >= 0 || i >= 0 || overflow == 1)
 	{
-		sum = carry;
-		if (i >= 0)
-			sum += n1[i] - '0';
-		if (j >= 0)
-			sum += n2[j] - '0';
-
-		r[--size_r] = (sum % 10) + '0';
-
-		carry = sum / 10;
-
-		i--;
+		if (i < 0)
+			val1 = 0;
+		else
+			val1 = *(n1 + i) - '0';
+		if (j < 0)
+			val2 = 0;
+		else
+			val2 = *(n2 + j) - '0';
+		temp_tot = val1 + val2 + overflow;
+		if (temp_tot >= 10)
+			overflow = 1;
+		else
+			overflow = 0;
+		if (digits >= (size_r - 1))
+			return (0);
+		*(r + digits) = (temp_tot % 10) + '0';
+		digits++;
 		j--;
+		i--;
 	}
-
-	if (carry)
-		r[--size_r] = carry + '0';
-
-	reverse_string(r + size_r, _strlen(r + size_r));
-
-	return (r + size_r);
+	if (digits == size_r)
+		return (0);
+	*(r + digits) = '0';
+	rev_string(r);
+	return (r);
 }
-
