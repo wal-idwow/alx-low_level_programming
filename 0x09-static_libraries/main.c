18e2a7b7b81529fcd6c4198c9b5469a79c13c726
walid-wow <medali25tech@gmail.com>
2023-08-04 21:17:37 +0000

    done

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,7 +1,13 @@
 #include "main.h"
 
+/**
+	* main - entry point
+	* Description: put string
+	* Return: 0 (Success)
+*/
+
 int main(void)
 {
-    _puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
-    return (0);
+_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
+	return (0);
 }
