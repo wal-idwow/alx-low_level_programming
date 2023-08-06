398f9561411079d1e3fca297217b37d93a157ba2
walid-wow <medali25tech@gmail.com>
2023-08-06 12:49:16 +0000

    done

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
-	* mzin - prints name of the program
+	* main - prints name of the program
 	* @argc: number of arguments
 	* @argv: array of arguments
 	* Return: Always 0 (Success)
