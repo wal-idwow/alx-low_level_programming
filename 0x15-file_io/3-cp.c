9db201320d6a4bbad61f96f5087ea69f4720ddab
wal-idwow <medali25tech@gmail.com>
2023-10-02 22:34:19 +0000

    done

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,45 +1,57 @@
-#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/stat.h>
 
-#define PERMISSIONS (S_IROTH | S_IWUSR | S_IRUSR | S_IRGRP | S_IWGRP)
-#define ERR_NOREAD "Error: Can't read from file %s\n"
-#define ERR_NOWRITE "Error: Can't write to %s\n"
-#define ERR_NOCLOSE "Error: Can't close fl %d\n"
-#define USAGE "Usage: cp file_from file_to\n"
+#define BUFFER_SIZE 1024
 
 /**
-	* main - programme
-	* @arc: argument count
-	* @arv: argument vector
-	* Return: 1 on success 0 on failure
+	* main - Programme
+	* @argc: argument account
+	* @argv: argument vector
+	* Return: 0 success
 */
 
-int main(int arc, char **arv)
+int main(int argc, char *argv[])
 {
-	int from_fl = 0, to_fl = 0;
-	ssize_t a;
-	char buf[READ_BUF_SIZE];
+	if (argc != 3)
+	{
+		fprintf(stderr, "Usage: %s file_from file_to\n", argv[0]);
+		return (97);
+	}
 
-	if (arc != 3)
-		dprintf(STDERR_FILENO, USAGE), exit(97);
-	from_fl = open(arv[1], O_RDONLY);
-	if (from_fl == -1)
-		dprintf(STDERR_FILENO, ERR_NOREAD, arv[1]), exit(98);
-	to_fl = open(arv[2], O_WRONLY | O_CREAT | O_TRUNC, PERMISSIONS);
-	if (to_fl == -1)
-		dprintf(STDERR_FILENO, ERR_NOWRITE, arv[2]), exit(99);
+	int fd_from, fd_to;
+	char buffer[BUFFER_SIZE];
+	ssize_t n;
 
-	while ((a = read(from_fl, buf, READ_BUF_SIZE)) > 0)
-		if (write(to_fl, buf, a) != a)
-			dprintf(STDERR_FILENO, ERR_NOWRITE, arv[2]), exit(99);
-	if (a == -1)
-		dprintf(STDERR_FILENO, ERR_NOREAD, arv[1]), exit(98);
+	fd_from = open(argv[1], O_RDONLY);
+	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0644);
 
-	from_fl = close(from_fl);
-	to_fl = close(to_fl);
-	if (from_fl)
-		dprintf(STDERR_FILENO, ERR_NOCLOSE, from_fl), exit(100);
-	if (to_fl)
-		dprintf(STDERR_FILENO, ERR_NOCLOSE, to_fl), exit(100);
+	if (fd_from == -1 || fd_to == -1)
+	{
+		perror("Error");
+		return (fd_from == -1 ? 98 : 99);
+	}
 
-	return (EXIT_SUCCESS);
+	while ((n = read(fd_from, buffer, BUFFER_SIZE)) > 0)
+	{
+		if (write(fd_to, buffer, n) != n)
+		{
+			perror("Error");
+			return (99);
+		}
+	}
+
+	if (n == -1)
+	{
+		perror("Error");
+		return (98);
+	}
+
+	close(fd_from);
+	close(fd_to);
+
+	return (0);
 }
+
