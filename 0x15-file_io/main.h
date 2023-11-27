7b5f30a4d1348f60cf2e37038c66c3af1e90cd14
wal-idwow <medali25tech@gmail.com>
2023-11-27 23:22:06 +0100

    File I/O

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -10,8 +10,9 @@
 
 #define READ_BUF_SIZE 1024
 
-ssize_t read_textfile(const char *filename, size_t letters);
 int _putchar(char c);
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
 
 
 
