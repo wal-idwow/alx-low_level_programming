64839c9dd90d3da60e661c295a6d1e67aafce2c8
wal-idwow <medali25tech@gmail.com>
2023-11-27 23:25:37 +0100

    File I/O

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+	* append_text_to_file - appends text to a file
+	* @filename: name of the file
+	* @text_content: text to append to the file
+	* Return: 1 on success, -1 on failure
+*/
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd, bytes_written;
+
+	/* Check if the filename or text_content is NULL */
+	if (!filename || !text_content)
+		return (-1);
+
+	/* Open the file for writing and appending */
+	fd = open(filename, O_WRONLY | O_APPEND);
+
+	/* Check if opening the file failed */
+	if (fd == -1)
+		return (-1);
+
+	/* Write text_content to the file */
+	bytes_written = write(fd, text_content, strlen(text_content));
+
+	/* Check if writing to the file failed */
+	if (bytes_written == -1)
+	{
+		close(fd);
+		return (-1);
+	}
+	close(fd);
+
+	/* Return 1 to indicate success */
+	return (1);
+}
+
