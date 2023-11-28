194f1c8ab85f1a36e202cc80ee61e580c1cd6775
wal-idwow <medali25tech@gmail.com>
2023-11-28 01:22:08 +0100

    File I/O

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -8,22 +8,34 @@
 */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int op;
-	int wr;
-	int len = 0;
+	int wrr; /*result of the write operation*/
+	int file; /*file descriptor for the file*/
+	int num_lett; /*number of letters in the text_content*/
 
-	if (filename == NULL)
-	{
-		for (len = 0; text_content[len]; len++)
-			;
-	}
-
-	op = open(filename, O_WRONLY | O_APPEND);
-	wr = write(op, text_content, len);
-
-	if (op == -1 || wr == -1)
+	if (!filename)
 		return (-1);
 
-	close(op);
+	/*open the file for writing and appending*/
+	file = open(filename, O_WRONLY | O_APPEND);
+
+	/*check if opening the file failed*/
+	if (file == -1)
+		return (-1);
+
+	/*check if text_content is not NULL*/
+	if (text_content)
+	{
+		/*calculate the number of letters in text_content*/
+		for (num_lett = 0; text_content[num_lett]; num_lett++)
+			;
+		/*write text_content to the file*/
+		wrr = write(file, text_content, num_lett);
+
+		/*check if writing to the file failed*/
+		if (wrr == -1)
+			return (-1);
+	}
+
+	close(file);
 	return (1);
 }
