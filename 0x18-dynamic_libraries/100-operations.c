906b93d01939314e50d7239cb1ac78509b6a9b33
wal-idwow <medali25tech@gmail.com>
2024-01-15 12:51:20 +0100

    done

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+int div(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error: Division/0\n");
+		return (0);
+	}
+	return (a / b);
+}
+
+int mod(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error/0\n");
+		return (0);
+	}
+	return (a % b);
+}
