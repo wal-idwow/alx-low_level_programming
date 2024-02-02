ed44c51aca3b1f99f96a0742f747c25e2e6e7d62
wal-idwow <medali25tech@gmail.com>
2024-02-02 23:26:42 +0100

    mandatory

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - retrieves a value associated with a key.
+ * @key: key to value
+ * @ht: hash table pointer
+ * Return: on success > value associated with key int hash table
+ *          on failure > NULL
+*/
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int i;
+	hash_node_t *n;
+
+	i = key_index((const unsigned char *)key, ht->size);
+
+	if (key == NULL || ht == NULL || *key == '\0')
+		return (NULL);
+
+	if (i >= ht->size)
+		return (NULL);
+
+	n = ht->array[i];
+	while (n && strcmp(n->key, key) != 0)
+		n = n->next;
+
+	return ((n == NULL) ? NULL : n->value);
+}
---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,37 @@
+#include "hash_tables.h"
+
+/**
+* hash_table_print -  prints a hash table.
+* @ht: hash table pointer
+* Description: print the key - value in order
+*/
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *n;
+    unsigned char symb = 0;
+	unsigned long int i;
+
+	if (ht == NULL)
+		return;
+
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		if (ht->array[i] != NULL)
+		{
+			if (symb == 1)
+				printf(", ");
+
+			n = ht->array[i];
+			while (n != NULL)
+			{
+				printf("'%s': '%s'", n->key, n->value);
+				n = n->next;
+				if (n != NULL)
+					printf(", ");
+			}
+			symb = 1;
+		}
+	}
+	printf("}\n");
+}
