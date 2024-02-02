3800678a89e29bdc63614fbc9aa4c365a7cf1814
wal-idwow <medali25tech@gmail.com>
2024-02-02 23:37:33 +0100

    mandatory

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,21 +1,24 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_get - retrieves a value associated with a key.
- * @key: key to value
- * @ht: hash table pointer
- * Return: on success > value associated with key int hash table
- *          on failure > NULL
+* hash_table_get - retrieves a value associated with a key.
+* @key: key to value
+* @ht: hash table pointer
+* Return: on success > value associated with key int hash table
+*          on failure > NULL
 */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	unsigned long int i;
+
 	hash_node_t *n;
 
-	i = key_index((const unsigned char *)key, ht->size);
 
 	if (key == NULL || ht == NULL || *key == '\0')
 		return (NULL);
+
+	i = key_index((const unsigned char *)key, ht->size);
+
 
 	if (i >= ht->size)
 		return (NULL);
--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -8,13 +8,14 @@
 void hash_table_print(const hash_table_t *ht)
 {
 	hash_node_t *n;
-    unsigned char symb = 0;
+	unsigned char symb = 0;
 	unsigned long int i;
 
 	if (ht == NULL)
 		return;
 
 	printf("{");
+
 	for (i = 0; i < ht->size; i++)
 	{
 		if (ht->array[i] != NULL)
